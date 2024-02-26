1ff2220243dbfe01dd156b2c8d50df498dbe00f3
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2024-02-26 16:36:43 -0500

    finished prob5

--- hog/hog.py
+++ hog/hog.py
@@ -176,6 +176,16 @@ def play(strategy0, strategy1, update,
     who = 0  # Who is about to take a turn, 0 (first) or 1 (second)
     # BEGIN PROBLEM 5
     "*** YOUR CODE HERE ***"
+    while score0 < goal and score1 < goal:
+        if who == 0:
+            roll_times = strategy0(score0, score1)
+            # remember to store the updated data to a iterable variable or 
+            # there will be an ifinite loop
+            score0 = update(roll_times, score0, score1, dice)
+        elif who == 1:
+            roll_times = strategy1(score1, score0)
+            score1 = update(roll_times, score1, score0, dice)
+        who = 1 - who
     # END PROBLEM 5
     return score0, score1
 
