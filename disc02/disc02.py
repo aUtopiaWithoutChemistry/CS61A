c9835e7d3f36220857a2158e7943fd3c3156ecc5
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2024-03-11 16:40:05 -0400

    finished Q3

--- disc02/disc02.py
+++ disc02/disc02.py
@@ -23,4 +23,22 @@ def make_keeper(n):
     return func
     
 
+def find_digit(k):
+    """Returns a function that returns the kth digit of x.
 
+    >>> find_digit(2)(3456)
+    5
+    >>> find_digit(2)(5678)
+    7
+    >>> find_digit(1)(10)
+    0
+    >>> find_digit(4)(789)
+    0
+    """
+    assert k > 0
+    "*** YOUR CODE HERE ***"
+    def func(x):
+        for i in range(k - 1):
+            x = x // 10
+        return x % 10
+    return func
