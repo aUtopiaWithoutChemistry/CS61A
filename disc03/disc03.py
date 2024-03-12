68e08b8157928b1b632dbe419f05d6054f336537
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2024-03-11 20:15:52 -0400

    Q4 finished

--- disc03/disc03.py
+++ disc03/disc03.py
@@ -60,3 +60,40 @@ def is_prime(n):
         else:
             return True
     return f(2)
+
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, 
+    and return the number of elements in the sequence.
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    print(n)
+    if n % 2 == 0:
+        return even(n)
+    else:
+        return odd(n)
+
+# using even and odd function to count how many times they
+# have been called
+def even(n):
+    return 1 + hailstone(n // 2)
+
+def odd(n):
+    "*** YOUR CODE HERE ***"
+    if n != 1:
+        return 1 + hailstone(n * 3 + 1)
+    else:
+        return 1
